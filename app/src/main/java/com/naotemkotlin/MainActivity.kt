59856b084ce9bca3830074100273c87fcd417445
Victor Ferreira <naotemerro@hotmail.com>
2024-03-17 13:12:11 -0300

    Alteração V.01

--- app/src/main/java/com/naotemkotlin/MainActivity.kt
+++ app/src/main/java/com/naotemkotlin/MainActivity.kt
@@ -2,7 +2,7 @@ package com.naotemkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//Comentário de alteração v.01
+//Comentário de alteração v.01.1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
